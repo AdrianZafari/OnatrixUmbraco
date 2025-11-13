ca7bd50ec7632e9bdc020770b1d656795c502195
AdrianZafari <adrian006@hotmail.com>
2025-11-13 01:11:58 +0100

    added callback elements

---
+++ UmbracoCMS/ViewModels/CallbackFormViewModel .cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UmbracoCMS.ViewModels;
+
+public class CallbackFormViewModel
+{
+    [Required(ErrorMessage = "Name is required")]
+    [Display(Name = "Name")]
+    [RegularExpression(@"^[A-Za-zÀ-ÖØ-öø-ÿ]+(?:[-'\s][A-Za-zÀ-ÖØ-öø-ÿ]+)*$", ErrorMessage = "Name is invalid")]
+    public string Name { get; set; } = null!;
+    
+    [Required(ErrorMessage = "Email is required")]
+    [Display(Name = "Email address")]
+    [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Invalid email address")]
+    public string Email { get; set; } = null!;
+    
+    [Required(ErrorMessage = "Phone number is required")]
+    [Display(Name = "Phone")]
+    [RegularExpression(@"^\+?\d{1,3}?[-.\s]?(?:\(?\d{1,4}\)?[-.\s]?)*\d{1,4}$", ErrorMessage = "Phone number is invalid")]
+    public string Phone { get; set; } = null!;
+
+    [Required(ErrorMessage = "Please select an option")]
+    public string SelectedOption { get; set; } = null!;
+}
