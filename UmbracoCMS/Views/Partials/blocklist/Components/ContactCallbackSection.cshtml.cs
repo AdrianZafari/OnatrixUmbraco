08c838b9ff8ddfb7ee5caa9e391af6b3af0750c0
AdrianZafari <adrian006@hotmail.com>
2025-11-15 18:11:09 +0100

    added form to contact page

---
+++ UmbracoCMS/Views/Partials/blocklist/Components/ContactCallbackSection.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UmbracoCMS.Views.Partials.blocklist.Components
+{
+    public class ContactCallbackSectionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
